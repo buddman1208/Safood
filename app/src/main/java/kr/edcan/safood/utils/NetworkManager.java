d9302d05a4d0ce1b13793408d96d2f97db96b49c
Junseok Oh <kotohana5706@edcan.kr>
2016-07-29 16:37:39 +0900

    Implement Data-Networking, Helper. Closes #2, #5.

---
+++ app/src/main/java/kr/edcan/safood/utils/NetworkManager.java
@@ -0,0 +1,12 @@
+/*
+ * Created by KOHA on 2016.
+ * Copyright by Sunrin Internet High School EDCAN
+ * All rights reversed.
+ */
+
+package kr.edcan.safood.utils;
+
+public interface NetworkManager {
+
+
+}
---
+++ app/src/main/java/kr/edcan/safood/utils/SafoodHelper.java
@@ -0,0 +1,18 @@
+
+/*
+ * Created by KOHA on 2016.
+ * Copyright by Sunrin Internet High School EDCAN
+ * All rights reversed.
+ */
+
+package kr.edcan.safood.utils;
+
+import android.content.Context;
+
+public class SafoodHelper {
+
+    private Context c;
+    public SafoodHelper(Context c) {
+        this.c = c;
+    }
+}
