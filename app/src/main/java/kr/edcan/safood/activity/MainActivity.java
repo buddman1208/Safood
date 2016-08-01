6075a6d2921bd5c1884977e3906754ee063fc211
Junseok Oh <kotohana5706@edcan.kr>
2016-08-01 16:49:58 +0900

    MainActivity: Implement CoordinatorLayout, Scrolling Behaviour

--- app/src/main/java/kr/edcan/safood/activity/MainActivity.java
+++ app/src/main/java/kr/edcan/safood/activity/MainActivity.java
@@ -1,5 +1,10 @@
 package kr.edcan.safood.activity;
 
+import android.animation.ObjectAnimator;
+import android.animation.StateListAnimator;
+import android.os.Build;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +12,19 @@ import kr.edcan.safood.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TabLayout tabLayout;
+    AppBarLayout appBarLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setDefault();
+    }
+
+    private void setDefault() {
+        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbarlayout);
+
     }
 }
