ac999f65eff208f1ebe89b7798b18522f65ded58
Junseok Oh <kotohana5706@edcan.kr>
2016-10-19 03:27:36 +0900

    Implement GroupGenerateActivity

--- app/src/main/java/kr/edcan/safood/activity/GroupGenerateActivity.java
+++ app/src/main/java/kr/edcan/safood/activity/GroupGenerateActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import kr.edcan.safood.R;
 
-public class GroupSetActivity extends AppCompatActivity {
+public class GroupGenerateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_set);
+        setContentView(R.layout.activity_group_generate);
     }
 }
