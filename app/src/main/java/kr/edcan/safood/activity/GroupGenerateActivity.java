c1062174a77a3f1b8cd7f126c19ada4348768846
Junseok Oh <kotohana5706@edcan.kr>
2016-10-19 13:39:34 +0900

    GroupGenerateActivity

--- app/src/main/java/kr/edcan/safood/activity/GroupGenerateActivity.java
+++ app/src/main/java/kr/edcan/safood/activity/GroupGenerateActivity.java
@@ -1,15 +1,31 @@
 package kr.edcan.safood.activity;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import kr.edcan.safood.R;
+import kr.edcan.safood.databinding.ActivityGroupGenerateBinding;
+import kr.edcan.safood.models.Group;
+import kr.edcan.safood.utils.NetworkHelper;
+import kr.edcan.safood.utils.StringUtils;
+import retrofit2.Call;
 
 public class GroupGenerateActivity extends AppCompatActivity {
 
+    ActivityGroupGenerateBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_generate);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_group_generate);
+        binding.groupGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(StringUtils.fullFilled(binding.groupNameInput)){
+//                    Call<Group> generateGroup = NetworkHelper.getNetworkInstance().
+                }
+            }
+        });
     }
 }
--- app/src/main/java/kr/edcan/safood/activity/MainActivity.java
+++ app/src/main/java/kr/edcan/safood/activity/MainActivity.java
@@ -154,8 +154,8 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     private void setDefault() {
         manager = new DataManager(getApplicationContext());
         if(manager.getActiveUser().second.getGroupid().equals("")) {
-            startActivity(new Intent(getApplicationContext(), GroupSetActivity.class));
-            finish();
+//            startActivity(new Intent(getApplicationContext(), GroupSetActivity.class));
+//            finish();
         }
         helper = new SafoodHelper(getApplicationContext());
         pager = (ViewPager) findViewById(R.id.mainPager);
