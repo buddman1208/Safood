cd7266feb7a3f9bb56784769bc169255d5c3b941
Junseok Oh <kotohana5706@edcan.kr>
2016-10-20 01:43:29 +0900

    Show GroupMemo Grid

--- app/src/main/java/kr/edcan/safood/activity/MainActivity.java
+++ app/src/main/java/kr/edcan/safood/activity/MainActivity.java
@@ -10,6 +10,10 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,21 +21,30 @@ import android.widget.ExpandableListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.github.nitrico.lastadapter.BR;
+import com.github.nitrico.lastadapter.LastAdapter;
+
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.Date;
 
 import kr.edcan.safood.R;
 import kr.edcan.safood.adapter.MainSafoodAdapter;
 import kr.edcan.safood.databinding.ActivityMainBinding;
+import kr.edcan.safood.databinding.GroupmemoGridBinding;
+import kr.edcan.safood.databinding.MainGroupmemoBinding;
 import kr.edcan.safood.databinding.MainSafoodBinding;
 import kr.edcan.safood.databinding.MainSearchBinding;
+import kr.edcan.safood.models.GroupMemo;
 import kr.edcan.safood.models.SafoodContentData;
 import kr.edcan.safood.models.SafoodTitleData;
 import kr.edcan.safood.utils.DataManager;
 import kr.edcan.safood.utils.SafoodHelper;
+import kr.edcan.safood.views.CartaTagView;
 import kr.edcan.safood.views.SlidingExpandableListView;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     String[] titleArr = new String[]{"안전한 음식", "그룹 메모", "음식 백과", "내 정보"};
 
@@ -61,7 +74,7 @@ public class MainActivity extends AppCompatActivity{
 
     private void setDefault() {
         manager = new DataManager(getApplicationContext());
-        if(manager.getActiveUser().second.getGroupid().equals("")) {
+        if (manager.getActiveUser().second.getGroupid().equals("")) {
 //            startActivity(new Intent(getApplicationContext(), GroupSetActivity.class));
 //            finish();
         }
@@ -84,8 +97,6 @@ public class MainActivity extends AppCompatActivity{
             }
         });
     }
-
-
 
 
     public static class MainFragment extends Fragment {
@@ -122,11 +133,11 @@ public class MainActivity extends AppCompatActivity{
                     view = inflater.inflate(R.layout.main_info, container, false);
                     break;
             }
-            setPage(view, getArguments().getInt(ARG_SECTION_NUMBER), inflater);
+            setPage(view, container, getArguments().getInt(ARG_SECTION_NUMBER), inflater);
             return view;
         }
 
-        private void setPage(View view, int position, LayoutInflater inflater) {
+        private void setPage(View view, ViewGroup container, int position, LayoutInflater inflater) {
             switch (position) {
                 case 0:
                     final SlidingExpandableListView listview = (SlidingExpandableListView) view.findViewById(R.id.mainExpandableListView);
@@ -166,10 +177,46 @@ public class MainActivity extends AppCompatActivity{
                         }
                     });
                     break;
+                case 1:
+                    CartaTagView newMemo = (CartaTagView) view.findViewById(R.id.newMemo);
+                    newMemo.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            startActivity(new Intent(v.getContext(), NewGroupMemoActivity.class));
+                        }
+                    });
+                    RecyclerView recyclerView= (RecyclerView) view.findViewById(R.id.recyclerview);
+                    ArrayList<GroupMemo> arrayList = new ArrayList<>();
+                    arrayList.add(new GroupMemo("asdf"));
+                    arrayList.add(new GroupMemo("asdf"));
+                    arrayList.add(new GroupMemo("asdf"));
+                    arrayList.add(new GroupMemo("asdf"));
+                    recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+                    LastAdapter.with(arrayList, BR._all)
+                            .layoutHandler(new LastAdapter.LayoutHandler() {
+                                @Override
+                                public int getItemLayout(@NotNull Object o, int i) {
+                                    return R.layout.groupmemo_grid;
+                                }
+                            })
+                            .onBindListener(new LastAdapter.OnBindListener() {
+                                @Override
+                                public void onBind(@NotNull Object o, @NotNull View view, int i, int i1) {
+                                    GroupmemoGridBinding binding = DataBindingUtil.getBinding(view);
+                                    binding.title.setText(((GroupMemo) o).getName());
+                                }
+                            })
+                            .onClickListener(new LastAdapter.OnClickListener() {
+                                @Override
+                                public void onClick(@NotNull Object o, @NotNull View view, int i, int i1) {
+                                    Log.e("asdf", i1 + "");
+                                }
+                            })
+                            .into(recyclerView);
+                    break;
             }
         }
     }
-
 
 
     public class MainPagerAdapter extends FragmentPagerAdapter {
---
+++ app/src/main/java/kr/edcan/safood/models/GroupMemo.java
@@ -0,0 +1,16 @@
+package kr.edcan.safood.models;
+
+/**
+ * Created by JunseokOh on 2016. 10. 20..
+ */
+public class GroupMemo {
+    private String name;
+
+    public GroupMemo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
