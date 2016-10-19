4e871bbb2eea9d8138bc6638edfa6e3c69d07e22
Junseok Oh <kotohana5706@edcan.kr>
2016-10-19 09:28:02 +0900

    GroupSetActivity Set Activity Flow

--- app/src/main/java/kr/edcan/safood/activity/GroupSetActivity.java
+++ app/src/main/java/kr/edcan/safood/activity/GroupSetActivity.java
@@ -1,15 +1,36 @@
 package kr.edcan.safood.activity;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import kr.edcan.safood.R;
+import kr.edcan.safood.databinding.ActivityGroupSetBinding;
 
-public class GroupSetActivity extends AppCompatActivity {
+public class GroupSetActivity extends AppCompatActivity implements View.OnClickListener {
 
+    ActivityGroupSetBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_set);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_group_set);
+        binding.joinGroup.setOnClickListener(this);
+        binding.makeGroup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.joinGroup:
+                startActivity(new Intent(getApplicationContext(), GroupSearchActivity.class));
+                finish();
+                break;
+            case R.id.makeGroup:
+                startActivity(new Intent(getApplicationContext(), GroupGenerateActivity.class));
+                finish();
+                break;
+        }
     }
 }
